e830d915934e6e8cab8a9617ba4437769d82640e
Казаченко Константин <mumkonuf@gmail.com>
2025-11-24 19:20:12 +0300

    upd

--- 11/mainwindow.cpp
+++ 11/mainwindow.cpp
@@ -18,98 +18,167 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_Plus_clicked()
 {
-    QString s;
-    double x1, x2, x3;
-    x1 = ui->lineEdit->text().toDouble();
-    x2 = ui->lineEdit_2->text().toDouble();
-    x3=x1+x2;
-    s.sprintf("%10.5lf",x3); ui->lineEdit_3->setText(s);
+    firstNumber = ui->lineEdit->text().toDouble();
+    pendingOperation = "+";
+    ui->lineEdit->clear();
 }
-
 
 void MainWindow::on_pushButton_Min_clicked()
 {
-    QString s;
-    double x1, x2, x3;
-    x1 = ui->lineEdit->text().toDouble();
-    x2 = ui->lineEdit_2->text().toDouble();
-    x3=x1-x2;
-    s.sprintf("%10.5lf",x3); ui->lineEdit_3->setText(s);
+    firstNumber = ui->lineEdit->text().toDouble();
+    pendingOperation = "-";
+    ui->lineEdit->clear();
 }
-
 
 void MainWindow::on_pushButton_um_clicked()
 {
-    QString s;
-    double x1, x2, x3;
-    x1 = ui->lineEdit->text().toDouble();
-    x2 = ui->lineEdit_2->text().toDouble();
-    x3=x1*x2;
-    s.sprintf("%10.5lf",x3); ui->lineEdit_3->setText(s);
+    firstNumber = ui->lineEdit->text().toDouble();
+    pendingOperation = "*";
+    ui->lineEdit->clear();
 }
-
 
 void MainWindow::on_pushButton_r_clicked()
 {
-    QString s;
-    double x1, x2, x3;
-    x1 = ui->lineEdit->text().toDouble();
-    x2 = ui->lineEdit_2->text().toDouble();
-    x3=x1/x2;
-    s.sprintf("%10.5lf",x3); ui->lineEdit_3->setText(s);
+    firstNumber = ui->lineEdit->text().toDouble();
+    pendingOperation = "/";
+    ui->lineEdit->clear();
 }
+
 
 
 void MainWindow::on_pushButton_kvk_clicked()
 {
     QString s;
-    double x1, x2, x3;
-    x1 = ui->lineEdit_4->text().toDouble();
-    x2 = sqrt(x1);
-    s.sprintf("%10.5lf",x2); ui->lineEdit_5->setText(s);
+    double x = ui->lineEdit->text().toDouble();
+    double r = sqrt(x);
+    s.sprintf("%10.5lf", r);
+    ui->lineEdit->setText(s);
 }
 
 
 void MainWindow::on_pushButton_tan_clicked()
 {
     QString s;
-    double x1, x2, x3;
-    x1 = ui->lineEdit_4->text().toDouble();
-    x2 = tan(x1);
-    s.sprintf("%10.5lf",x2); ui->lineEdit_5->setText(s);
+    double x = ui->lineEdit->text().toDouble();
+    double r = tan(x);
+    s.sprintf("%10.5lf", r);
+    ui->lineEdit->setText(s);
 }
 
 void MainWindow::on_pushButton_log_clicked()
 {
     QString s;
-    double x1, x2, x3;
-    x1 = ui->lineEdit_4->text().toDouble();
-    x2 = log(x1);
-    s.sprintf("%10.5lf",x2); ui->lineEdit_5->setText(s);
+    double x = ui->lineEdit->text().toDouble();
+    double r = log(x);
+    s.sprintf("%10.5lf", r);
+    ui->lineEdit->setText(s);
 }
+
 
 void MainWindow::on_pushButton_factorial_clicked()
 {
     QString s;
-    int n;
+    int n = ui->lineEdit->text().toInt();
     long long result = 1;
 
-    // Получаем число из lineEdit
-    n = ui->lineEdit_4->text().toInt();
-
-    // Проверяем, что число неотрицательное
     if (n < 0) {
-        ui->lineEdit_5->setText("Ошибка: отрицательное число");
+        ui->lineEdit->setText("Ошибка");
         return;
     }
 
-    // Вычисляем факториал
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
         result *= i;
-    }
 
-    // Форматируем результат и выводим
     s.sprintf("%lld", result);
-    ui->lineEdit_5->setText(s);
+    ui->lineEdit->setText(s);
 }
 
+
+void MainWindow::on_pushButton_0_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "0");
+}
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "1");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "2");
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "3");
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "4");
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "5");
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "6");
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "7");
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "8");
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    ui->lineEdit->setText("");
+    ui->lineEdit->setText(ui->lineEdit->text() + "9");
+}
+
+
+void MainWindow::on_pushButton_dot_clicked()
+{
+    QString text = ui->lineEdit->text();
+    if (!text.contains("."))
+        ui->lineEdit->setText(text + ".");
+}
+
+
+void MainWindow::on_pushButton_equals_clicked()
+{
+    QString s;
+    double secondNumber = ui->lineEdit->text().toDouble();
+    double result = 0;
+
+    if (pendingOperation == "+") result = firstNumber + secondNumber;
+    if (pendingOperation == "-") result = firstNumber - secondNumber;
+    if (pendingOperation == "*") result = firstNumber * secondNumber;
+    if (pendingOperation == "/") result = firstNumber / secondNumber;
+
+    s.sprintf("%10.5lf", result);
+    ui->lineEdit->setText(s);
+}
+
+void MainWindow::on_pushButton_clear_clicked()
+{
+    ui->lineEdit->setText("");
+}
--- 11/mainwindow.h
+++ 11/mainwindow.h
@@ -30,7 +30,35 @@ private slots:
 
     void on_pushButton_factorial_clicked();
 
+    void on_pushButton_dot_clicked();
+
+    void on_pushButton_equals_clicked();
+
+    void on_pushButton_clear_clicked();
+
+    void on_pushButton_0_clicked();
+
+    void on_pushButton_1_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
 private:
     Ui::MainWindow *ui;
+    double firstNumber;
+    QString pendingOperation;
 };
 #endif
